ba40cf2fb1c54dc4d8ae3a9a9b29316c74667f38
oussama omrani <test01test14sx@gmail.com>
2023-03-04 17:46:41 +0100

    creating panic recover example

--- main.go
+++ main.go
@@ -2,11 +2,32 @@ package main
 
 import ("fmt"
 	"bufio"
-	"os")
+	"os"
+	"runtime/debug")
 func main() {
 	fmt.Println("enter a user name")
 	reader := bufio.NewReader(os.Stdin)
-	name, _ := reader.ReadString('\n')
-	fmt.Print("your name is ",name)
-
+	nom, _ := reader.ReadString('\n')
+	
+	fmt.Println("enterer prenom")
+	prenom, _ := reader.ReadString('\n')
+	fullname:= nom+prenom
+	namefield(nil,nil)
+	fmt.Print("your name is ",fullname)
+	
 }	
+func namefield(nom *string, prenom *string ){
+	defer recoverFullName()
+	if nom == nil{
+		panic("runtime error: first name cannot be null")
+	}
+	if prenom== nil{
+		panic("runtime error: prenom cannot be null")
+	}
+}
+func recoverFullName(){
+	if r:= recover();r!=nil{
+		fmt.Println("recovered from",r)
+		debug.PrintStack()
+	}
+}

f4dc493fde52914cc3ff6b2c9aad7c49b0081a9c
oussama omrani <test01test14sx@gmail.com>
2023-03-04 17:04:09 +0100

    creating a user input

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import ("fmt"
+	"bufio"
+	"os")
 func main() {
-	
+	fmt.Println("enter a user name")
+	reader := bufio.NewReader(os.Stdin)
+	name, _ := reader.ReadString('\n')
+	fmt.Print("your name is ",name)
 
 }	

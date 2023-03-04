069f2a85fa49c4b557272726317a00cb2906600b
oussama omrani <test01test14sx@gmail.com>
2023-03-04 18:43:43 +0100

    adding fibonacci to main

--- main.go
+++ main.go
@@ -1,33 +1,33 @@
 package main
 
-import ("fmt"
-	"bufio"
-	"os"
-	"runtime/debug")
+import (
+	"fmt"
+
+	"first_prog/exercices"
+	"runtime/debug"
+
+)
+
 func main() {
-	fmt.Println("enter a user name")
-	reader := bufio.NewReader(os.Stdin)
-	nom, _ := reader.ReadString('\n')
+	fmt.Println("enter a number")
+	var number int 
+	fmt.Scanf("%d",&number)
+	exercices.Fibo(number)
 	
-	fmt.Println("enterer prenom")
-	prenom, _ := reader.ReadString('\n')
-	fullname:= nom+prenom
-	namefield(nil,nil)
-	fmt.Print("your name is ",fullname)
-	
-}	
-func namefield(nom *string, prenom *string ){
+
+}
+func namefield(nom *string, prenom *string) {
 	defer recoverFullName()
-	if nom == nil{
+	if nom == nil {
 		panic("runtime error: first name cannot be null")
 	}
-	if prenom== nil{
+	if prenom == nil {
 		panic("runtime error: prenom cannot be null")
 	}
 }
-func recoverFullName(){
-	if r:= recover();r!=nil{
-		fmt.Println("recovered from",r)
+func recoverFullName() {
+	if r := recover(); r != nil {
+		fmt.Println("recovered from", r)
 		debug.PrintStack()
 	}
 }
